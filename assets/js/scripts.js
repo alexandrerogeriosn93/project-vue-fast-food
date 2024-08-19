eafd29e2efdc39f7a9b54fe22b35f3d30ce8b03a
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-08-19 20:10:05 -0300

    feat: add data binding

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,26 @@
-const vm = new Vue({ el: "#app" });
+const vm = new Vue({
+  el: "#app",
+  computed: {
+    bread() {
+      return [
+        "assets/img/default/pao_superior.png",
+        "assets/img/default/pao_inferior.png",
+      ];
+    },
+    lettuce() {
+      return "assets/img/default/alface.png";
+    },
+    ketchup() {
+      return "assets/img/default/molho.png";
+    },
+    mustard() {
+      return "assets/img/default/molho.png";
+    },
+    mayonnaise() {
+      return "assets/img/default/molho.png";
+    },
+    hamburger() {
+      return "assets/img/default/hamburguer.png";
+    },
+  },
+});
