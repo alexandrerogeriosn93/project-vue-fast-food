aadd853be6bc77c5d5fdf764db0d6d673516ed02
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-08-19 21:15:02 -0300

    feat: add function to select bread

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,27 @@
 const vm = new Vue({
   el: "#app",
+  data: {
+    inputTypeBread: "",
+  },
   computed: {
     bread() {
-      return [
-        "assets/img/default/pao_superior.png",
-        "assets/img/default/pao_inferior.png",
-      ];
+      switch (this.inputTypeBread) {
+        case "gergelim":
+          return [
+            "assets/img/pao_gergelim_superior.png",
+            "assets/img/pao_gergelim_inferior.png",
+          ];
+        case "australiano":
+          return [
+            "assets/img/pao_australiano_superior.png",
+            "assets/img/pao_australiano_inferior.png",
+          ];
+        default:
+          return [
+            "assets/img/default/pao_superior.png",
+            "assets/img/default/pao_inferior.png",
+          ];
+      }
     },
     lettuce() {
       return "assets/img/default/alface.png";
